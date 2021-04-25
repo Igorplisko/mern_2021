afe6bacc0ce4999f0480101b72feb901f700b509
Igorplisko <igorplisko95@gmail.com>
2021-04-25 23:59:24 +0300

    fist commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import 'materialize-css'
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      fwfwfgbdftnredbr
+    </div>
+  );
+}
+
+export default App
+cd
