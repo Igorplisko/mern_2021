6eef5c48a9705d254ebd177b57e2e7c60f4d03d4
Igorplisko <igorplisko95@gmail.com>
2021-04-26 00:07:03 +0300

    commit

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ async function start() {
 }
 start()
 
-//!!!!
+//!!!!!!!!!!!!!!!!!!!
 
 
--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="container">
       <h1>Hello</h1>
-      fwfwfgbdftnredbr
+
     </div>
   );
 }
 
 export default App
-cd
