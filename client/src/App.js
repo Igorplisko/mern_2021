aed78191a7ea958f2f50221581d00fe7c4222b56
Igorplisko <igorplisko95@gmail.com>
2021-04-26 20:44:44 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import { useRoutes } from './routes'
 import 'materialize-css'
 
 
 function App() {
+  const routes = useRoutes(false)
   return (
-    <div className="container">
-      <h1>Hello</h1>
-
-    </div>
-  );
+    <Router>
+      <div className="container">
+        {routes}
+      </div>
+    </Router>
+  )
 }
 
 export default App
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import { CreatePage } from './pages/CreatePaje';
-import { DetailPage } from './pages/DetailPage';
-import { LinksPage } from './pages/LinksPage';
-
-
+import { LinksPage } from './pages/LinksPage'
+import { CreatePage } from './pages/CreatePage'
+import { DetailPage } from './pages/DetailPage'
+import { AuthPage } from './pages/AuthPage'
 
 export const useRoutes = isAuthenticated => {
    if (isAuthenticated) {
@@ -23,12 +22,13 @@ export const useRoutes = isAuthenticated => {
          </Switch>
       )
    }
+
    return (
       <Switch>
-         <Route>
-            //!!!!!!!!!!!!!!!!!!!!!!!!!
+         <Route path="/" exact>
+            <AuthPage />
          </Route>
+         <Redirect to="/" />
       </Switch>
    )
-
 }
