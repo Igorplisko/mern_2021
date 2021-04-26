e57ab7ec23ff3b387a19aa6e1e88effe370061db
Igorplisko <igorplisko95@gmail.com>
2021-04-26 20:57:11 +0300

    commit

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-<<<<<<< HEAD
 import { LinksPage } from './pages/LinksPage'
 import { CreatePage } from './pages/CreatePage'
 import { DetailPage } from './pages/DetailPage'
 import { AuthPage } from './pages/AuthPage'
-=======
-import { AuthPage } from './pages/AuthPage';
-import { CreatePage } from './pages/CreatePaje';
-import { DetailPage } from './pages/DetailPage';
-import { LinksPage } from './pages/LinksPage';
 
-
->>>>>>> 660dca828a2a719e1f9e9d3d9de422e9e12b7aa6
 
 export const useRoutes = isAuthenticated => {
    if (isAuthenticated) {
       return (
+
          <Switch>
             <Route path="/links" exact>
                <LinksPage />
@@ -37,11 +30,7 @@ export const useRoutes = isAuthenticated => {
          <Route path="/" exact>
             <AuthPage />
          </Route>
-<<<<<<< HEAD
          <Redirect to="/" />
-=======
-         <Redirect to={ } />
->>>>>>> 660dca828a2a719e1f9e9d3d9de422e9e12b7aa6
       </Switch>
    )
 }
